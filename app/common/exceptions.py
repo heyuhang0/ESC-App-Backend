32f5583fba25512f8653193acee9d258c4798ac2
heyuhang0 <heyuhang0@outlook.com>
2020-03-15 09:19:20 +0800

    Flask-restful style error message

--- app/common/exceptions.py
+++ app/common/exceptions.py
@@ -8,14 +8,16 @@ class InvalidUsage(Exception):
         self.header = header
 
     def make_response(self):
-        content = {
-            'error': {
-                'message': self.message,
-                'type': self.__class__.__name__,
-                'code': self.status_code
-            }
-        }
-        res = make_response(jsonify(content), self.status_code)
+        res = make_response(jsonify({'message': self.message}), self.status_code)
         if self.header:
             res.headers.extend(self.header)
         return res
+
+
+class NotFoundException(InvalidUsage):
+    def __init__(self, obj):
+        if isinstance(obj, str):
+            message = obj
+        else:
+            message = obj.__name__ + ' not found'
+        super().__init__(message, 404)
